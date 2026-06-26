d4990430c4091b0a1e3c04f52d180cf3d8acd3bd
desoult1on <misha.vinogradov.1996@mail.ru>
2026-06-26 23:56:18 +0300

    Добавлена главная страница с товарами из API

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { fetchProducts } from "../api/api"
+import axios from "axios"
+function HomePage() {
+    const [products, setProducts] = useState([])
+useEffect(() => {
+     const loadProducts = async() =>{
+        const response = await fetchProducts()
+        setProducts(response.data)
+     }
+     loadProducts()
+},[])
+    return <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Товары</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {products.map(product => (
+                <div key={product.id} className="border p-4 rounded">
+                    <img src = {product.image} alt={product.title} className="h-48 w-full object-contain"/>
+                    <h2 className="font-semibold mt-2">{product.title}</h2>
+                    <p className="text-lg font-bold">{product.price} $</p>
+                    </div>
+                ))}    
+        </div>
+    </div>
+
+}
+export default  HomePage
