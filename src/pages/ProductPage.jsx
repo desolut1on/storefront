be2b4226d50515dddaa48420b7e58197b37dd474
desoult1on <misha.vinogradov.1996@mail.ru>
2026-06-29 15:46:38 +0300

    Добавлена кнопка 'Назад' на страницу товара с навигацией navigate(-1)

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,11 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { fetchProduct } from "../api/api"
 import { useEffect, useState } from "react"
 function ProductPage() {
+    const navigate = useNavigate()
+    const handleGoBack = () => {
+         navigate(-1)
+    }
     const {id} = useParams()
     const [product,setProduct] = useState(null)
     useEffect(()=> {
@@ -16,8 +20,14 @@ function ProductPage() {
     }
     return (
         <div className="max-w-4xl mx-auto p-4">
+            <button
+            onClick={handleGoBack}
+            className="mb-4 text-blue-600 hover:underline cursor-pointer"
+            >
+                 ← Назад
+            </button>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-
+                
                 <div className="flex items-center justify-center">
                     <img
                     src={product.image}
